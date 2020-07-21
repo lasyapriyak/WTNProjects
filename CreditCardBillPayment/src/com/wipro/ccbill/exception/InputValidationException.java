d8ef687a9780b3c6bf17e1a3f0d2477f37c0e8ea
Lasya <Lasya@DESKTOP-7SS5TJP>
2020-07-21 15:20:02 +0530

    First Commit

---
+++ CreditCardBillPayment/src/com/wipro/ccbill/exception/InputValidationException.java
@@ -0,0 +1,11 @@
+package com.wipro.ccbill.exception;
+
+public class InputValidationException extends Exception{
+	public String toString()
+	{
+		return "Invalid Input Data";
+	}
+	public InputValidationException(String str){
+		super(str);
+	}
+}
